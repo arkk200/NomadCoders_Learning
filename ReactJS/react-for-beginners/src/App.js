62c7613e57c9b96b537678292e82d37b8706b61e
arkk200 <arkk20000@gmail.com>
2022-07-20 02:55:00 +0900

    Practiced useEffect

--- ReactJS/react-for-beginners/src/App.js
+++ ReactJS/react-for-beginners/src/App.js
@@ -1,11 +1,35 @@
-import Button from './Button';
-import styles from './App.module.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onChange = (event) => setKeyword(event.target.value);
+  const onClick = () => setValue(prev => prev + 1);
+  console.log('I run all the time');
+  // useEffect(function, [dependence])
+  // dependence에 있는 값이 변할 때 안에 있는 함수가 실행됨
+  useEffect(() => {
+    console.log('I run only once.');
+  }, []);
+  useEffect(() => {
+      console.log('I run when keyword changes');
+  }, [keyword]);
+  useEffect(() => {
+    console.log('I run when counter changes');
+}, [counter]);
+useEffect(() => {
+  console.log('I run when keyword & counter change');
+}, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <input
+      value={keyword}
+      onChange={onChange}
+      type="text"
+      placeholder='Search here...'
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
