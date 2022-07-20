f6d1f753135024ff5819d57faf180725cfcf2c52
arkk200 <arkk20000@gmail.com>
2022-07-20 12:19:55 +0900

    Cleanup function run when component is destroyed

--- ReactJS/react-for-beginners/src/App.js
+++ ReactJS/react-for-beginners/src/App.js
@@ -1,35 +1,22 @@
 import {useState, useEffect} from 'react';
 
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("distroyed :("); // Cleanup function
+  }, [])
+  return (
+    <h1>Hello</h1>
+  );
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onChange = (event) => setKeyword(event.target.value);
-  const onClick = () => setValue(prev => prev + 1);
-  console.log('I run all the time');
-  // useEffect(function, [dependence])
-  // dependence에 있는 값이 변할 때 안에 있는 함수가 실행됨
-  useEffect(() => {
-    console.log('I run only once.');
-  }, []);
-  useEffect(() => {
-      console.log('I run when keyword changes');
-  }, [keyword]);
-  useEffect(() => {
-    console.log('I run when counter changes');
-}, [counter]);
-useEffect(() => {
-  console.log('I run when keyword & counter change');
-}, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(current => !current);
   return (
     <div>
-      <input
-      value={keyword}
-      onChange={onChange}
-      type="text"
-      placeholder='Search here...'
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
