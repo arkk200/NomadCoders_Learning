e3934346fac1e605374ff6cefce90559972fdc0e
arkk200 <arkk20000@gmail.com>
2022-08-06 17:10:24 +0900

    Chat Completed

--- NodeJS/zoom/src/public/js/app.js
+++ NodeJS/zoom/src/public/js/app.js
@@ -1,1 +1,42 @@
-alert('hi');
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+// websocket은 ws로 주소가 시작해야 한다.
+// 또한 모바일에선 localhost가 존재하지 않으므로
+// window.location.host를 써서 브라우저의 위치를 나타내준다.
+// window.location.host를 쓰면 포트 번호가 바뀌어도 작동한다.
+
+// app.js의 socket은 서버로의 연결을 뜻한다.
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// socket이 connection을 open 했을 때 이벤트 발생
+socket.addEventListener('open', () => {
+    console.log("Conneted to Server ♬");
+});
+
+// 백엔드에서 오는 메세지를 감지
+socket.addEventListener('message', message => {
+    // 프론트엔드에서 메세지를 받음
+    console.log("New message: ", message.data);
+});
+
+socket.addEventListener('close', () => {
+    console.log("Disconnted from Server ※")
+});
+
+/*
+setTimeout(() => {
+    // 프론트엔드에서도 메세지를 보낼 때 백엔드에서 보냈던 것처럼 하면 됨
+    socket.send("hello from the browser");
+}, 10000);
+*/
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector('input');
+    socket.send(input.value);
+    // console.log(input.value);
+    input.value = '';
+}
+
+messageForm.addEventListener('submit', handleSubmit);
--- NodeJS/zoom/src/server.js
+++ NodeJS/zoom/src/server.js
@@ -15,4 +15,31 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// fake database
+const sockets = [];
+
+// on method는 백엔드에 연결된 사람의 정보를 제공한다.
+// 정보는 socket을 통해서 옴
+
+// server.hs에 있는 socket은 연결된 브라우저를 뜻한다.
+wss.on("connection", socket => {
+    // 연결이 될 때마다 connection이벤트가 일어나고 sockets에 push 됨
+    sockets.push(socket);
+    console.log("Conneted to Browswer ♬");
+
+    // 브라우저가 다치면 close 이벤트가 감지되고 아래 코드가 실행된다.
+    socket.on("close", () => console.log("Disconnted from Browser ※"))
+
+    // 백엔드에서도 메세지를 받을 때 프론트엔드에서 받았던 것처럼 message 이벤트를 쓰면 됨
+    socket.on("message", (message) => {
+        // 브라우저 배열에서 차례대로 메세지를 보냄
+        sockets.forEach(aSocket => aSocket.send(message.toString('utf8')))
+
+        // socket.send(message.toString('utf8'));
+    });
+    
+    // 백엔드에서 메세지를 보냄
+    // socket.send("hello");
+});
+
 server.listen(3000, handleListen);
