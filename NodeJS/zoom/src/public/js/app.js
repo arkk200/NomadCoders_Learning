ddb762fbed3d107096ec73714b7828f1bd3c0309
arkk200 <arkk20000@gmail.com>
2022-08-07 08:59:39 +0900

    Recap

--- NodeJS/zoom/src/public/js/app.js
+++ NodeJS/zoom/src/public/js/app.js
@@ -1,7 +1,14 @@
+// socket.io의 장점 중 하나는 서버를 닫는다면
+// 웹 브라우저가 연결을 계속 시도할 수 있게 해준다는 것이다.
+
 const socket = io();
 
 const welcome = document.getElementById('welcome');
 const form = welcome.querySelector('form');
+
+function backendDone(msg) {
+    console.log(`The backend says: ${msg}`);
+}
 
 function handleRoomSubmit(event){
     event.preventDefault();
@@ -10,10 +17,12 @@ function handleRoomSubmit(event){
     // socket.io에선 어떤 이벤트든 전송할 수 있다.
     // socket.io의 또다른 장점은 websocket에서 못하던 JS 객체를 보내는 것을 할 수 있다.
     // 또한 메세지를 보낼 때 문자열로 바꿔줄 필요가 없다.
-    // 인자로는 (커스텀이벤트명, 보내고 싶은 데이터, 콜백함수)가 들어간다.
-    socket.emit("enter_room", {payload: input.value}, () => {
-        console.log("server is done!");
-    });
+    
+    // socket.emit의 인자의 첫번째는 이벤트 명, 그 뒤부턴 다양한 데이터를 보낼 수 있다.
+    // 요청이 끝난 뒤 실행할 함수를 쓰고 싶으면 그 함수는 꼭 마지막 인자에 와야 한다.
+    // 이 함수는 백엔드에서 실행되면 보안 문제가 생기기에 프론트엔드에서 실행된다.
+    // 그렇기에 백은 프론트에서 오는 코드를 실행시키면 안된다.
+    socket.emit("enter_room", input.value, backendDone);
     input.value = '';
 }
 
--- NodeJS/zoom/src/server.js
+++ NodeJS/zoom/src/server.js
@@ -28,10 +28,12 @@ socket.io로 넘어오며 발전된 점
 
 io.on("connection", socket => {
     // socket.on 뒤에 원하는 이벤트를 넣으면 된다.
-    socket.on("enter_room", (msg, done) => {
-        console.log(msg);
+    socket.on("enter_room", (roomName, done) => {
+        console.log(roomName);
         setTimeout(() => {
-            done();
+            // 프론트에서 실행되는 함수의 인자를 백에서 줄 수 있다.
+            done("hello from the backend"); // done 함수 내에 있는 코드는 프론트에서 실행됨
+            // socketIO의 또다른 좋은 점은 백에서 프론트의 함수를 실행시킬 수 있다는 것이다.
         }, 3000);
     });
 });
