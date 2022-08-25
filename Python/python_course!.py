c69a27a8e1c4c0ffef02f688730e3c2bdfe3e82f
arkk200 <arkk20000@gmail.com>
2022-08-25 13:13:27 +0900

    Parameters

--- Python/python_course!.py
+++ Python/python_course!.py
@@ -1,11 +1,10 @@
-# def로 함수를 정의할 수 있다.
-# 
-def say_hello():
-    print("hello how r u?")
+def say_hello(user_name): # 변수처럼 쓰면 된다. 이게 전달인자(parameter)이다.
+    # 쉼표(,)를 이용하여 문자열 사이에 데이터를 삽입할 수 있다.
+    print("hellow", user_name, "how r u?")
 
-# 괄호는 함수의 실행버튼이다.
-# 함수는 재사용이 가능하다.
-say_hello()
-say_hello()
-say_hello()
-say_hello()
+# 함수에 들어가는 데이터를 인자(argument)라고 한다.
+say_hello("arkk")
+say_hello("nico")
+say_hello("lynn")
+say_hello("lewis")
+say_hello("ralph")
