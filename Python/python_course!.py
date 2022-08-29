189c749c1003191a1ee30683ffa6ab1a9d2315f0
arkk200 <arkk20000@gmail.com>
2022-08-30 08:00:22 +0900

    Else & Elif

--- Python/python_course!.py
+++ Python/python_course!.py
@@ -1,8 +1,18 @@
-a = "testt"
+password_correct = False
 
-if a == "testt":
-    print("True!")
+if password_correct:
+    print("Here is your money")
+else: # else는 선택사항이다.
+    print("Wrong password")
 
-b = 10
-if b < 12:
-    print("Correct!")
+
+winner = 10
+
+# if나 elif의 조건이 True라면 그 뒤에 나오는 조건은 확인하지 않는다.
+if winner > 10:
+    print("Winner is greater than 10")
+# elif는 코드에 또다른 대안과 조건을 넣을 수 있게 해준다.
+elif winner < 10:
+    print("Winner is less than 10")
+else:
+    print("Winner is 10")
