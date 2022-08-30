62551d6a4519bf4c25c6eeeba72785c1832c5798
arkk200 <arkk20000@gmail.com>
2022-08-30 13:08:32 +0900

    And & Or

--- Python/python_course!.py
+++ Python/python_course!.py
@@ -1,18 +1,31 @@
-password_correct = False
+# input은 하나의 값만 받는다.
+age = int(input("How old are you?"))
 
-if password_correct:
-    print("Here is your money")
-else: # else는 선택사항이다.
-    print("Wrong password")
+print("user answer", age)
 
+# type()은 변수의 type을 알려주는 함수이다.
+print(type(age))
 
-winner = 10
-
-# if나 elif의 조건이 True라면 그 뒤에 나오는 조건은 확인하지 않는다.
-if winner > 10:
-    print("Winner is greater than 10")
-# elif는 코드에 또다른 대안과 조건을 넣을 수 있게 해준다.
-elif winner < 10:
-    print("Winner is less than 10")
+# 입력 받은 값을 숫자와 비교하기 위해서 입력 받은 string을 int로 바꿔줘야한다.
+if age < 18:
+    print("You can't drink.")
+    # and 연산자는 두 조건 중 하나라도 거짓이면 거짓이다.
+elif age >= 18 and age <= 35:
+    print("You drink beer!")
+    # or 연산자는 두 조건 중 하나라도 참이면 참이다.
+elif age == 60 or age == 70:
+    print("Birithday party!")
+elif age >= 75:
+    print("Dangerous to drink beer")
 else:
-    print("Winner is 10")
+    print("Go ahead!")
+
+True and True == True
+True and False == False
+False and True == False
+False and False == False
+
+True or True == True
+True or False == True
+False or True == True
+False or False == False
