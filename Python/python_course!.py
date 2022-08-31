e4634104c8df93d4374b97af025dfb1aeb499198
arkk200 <arkk20000@gmail.com>
2022-08-31 11:40:07 +0900

    Python Casino

--- Python/python_course!.py
+++ Python/python_course!.py
@@ -1,21 +1,16 @@
 from random import randint
-# 외부 라이브러리를 불러오려면 import 라는 명령어를 써야한다.
 
-user_choice = int(input("Choose number:"))
-# randint(a, b)는 a와 b사이에 정수 중 임의의 값 하나를 반환해준다.
-pc_choice = randint(1, 50)
+print("Welcome to Python Casino")
+pc_choice = randint(1, 100)
 
-if user_choice == pc_choice:
-    print("You won!")
-elif user_choice > pc_choice:
-    print("Lower! Computer chose", pc_choice)
-elif user_choice < pc_choice:
-    print("Higher! Computer chose", pc_choice)
+playing = True
 
-# while은 조건이 안 맞을 때까지 계속 출력하는 if 같은거다
-
-distance = 0
-# 0부터 1씩 증가하면서 출력하다가 값이 20이상이면 반복문을 끝낸다.
-while distance < 20:
-    print("I'm running:", distance, "km")
-    distance = distance + 1
+while playing:
+    user_choice = int(input("Choose number (1-100): "))
+    if user_choice == pc_choice:
+        print("You won!")
+        playing = False # 이겼다면 playing에 False를 대입해서 while문 빠져나옴
+    elif user_choice > pc_choice:
+        print("Lower!")
+    elif user_choice < pc_choice:
+        print("Higher!")
