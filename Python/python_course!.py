0d1b5500eee26dd00ec65fa0654f0e966c115205
arkk200 <arkk20000@gmail.com>
2022-09-01 09:54:31 +0900

    URL Formatting

--- Python/python_course!.py
+++ Python/python_course!.py
@@ -3,17 +3,15 @@
 websites = (
     "google.com",
     "airbnb.com",
-    "twitter.com",
+    "https://twitter.com",
     "facebook.com",
-    "youtube.com"
+    "https://youtube.com"
 )
 
-# 집합체라면 for in을 이용해서 개수 만큼 반복문을 돌릴 수 있다.
-# for, in 사이에 변수를 통해서 값을 가져온다.
-# 값을 가져오는 변수명은 튜플, 리스트명의 단수 형태로 쓴다.
-# 그러나 단어는 내 마음대로 정할 수 있다.
 for website in websites:
-    print("potato is equals to", website)
-
-for xxxx in websites:
-    print("potato is equals to", xxxx)
+    # not은 참, 거짓을 반전시킨다.
+    # 이 조건문은 'https://' 로 시작하지 않는 url에만 작동한다.
+    if not website.startswith("https://"):
+        website = f"https://{website}"
+    print(website)
+print(websites)
