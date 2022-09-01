fa25d8953d821e41a2691326e8da8ede6df95ad2
arkk200 <arkk20000@gmail.com>
2022-09-01 17:16:51 +0900

    BeautifulSoup

--- Python/job_scrapper.py
+++ Python/job_scrapper.py
@@ -1,11 +1,12 @@
 from requests import get
+from bs4 import BeautifulSoup
 
 base_url = "https://weworkremotely.com/remote-jobs/search?term="
 search_term = "python"
-# https://weworkremotely.com/remote-jobs/search?term=
 response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
     # .test을 이용하여 웹페이지의 html 코드를 가져올 수 있다.
-    print(response.text)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    jobs = print(soup.find_all('section', class_="jobs"))
