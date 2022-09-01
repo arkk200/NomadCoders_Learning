671a51a5a376b59c563fe60cdb12d52c603e00e9
arkk200 <arkk20000@gmail.com>
2022-09-01 10:20:31 +0900

    Status Codes

--- Python/python_course!.py
+++ Python/python_course!.py
@@ -1,3 +1,4 @@
+# pypi에 있는 라이브러리를 설치하면 이런식으로 쓸 수 있다.
 from requests import get
 
 websites = (
@@ -8,8 +9,17 @@ websites = (
     "https://youtube.com"
 )
 
+results = {}
+
 for website in websites:
     if not website.startswith("https://"):
         website = f"https://{website}"
-    print(website)
-print(websites)
+    response = get(website)
+    if response.status_code == 200:
+        results[website] = "OK"
+    else:
+        results[website] = "FAILED"
+print(results)
+
+# Response 옆에 숫자는 아래에 html status 코드를 보면 확인할 수 있다.
+# https://developer.mozilla.org/ko/docs/Web/HTTP/Status
