5c57022ba72c80d4da5f03d9454366cf96af1dfd
arkk200 <arkk20000@gmail.com>
2022-09-09 13:36:39 +0900

    None

--- Python/job_scrapper.py
+++ Python/job_scrapper.py
@@ -1,4 +1,3 @@
-from requests import get
 from bs4 import BeautifulSoup
 # 파이썬 파일을 따로 만들고 거기서 함수를 불러올 수도 있다.
 from extractors.wwr import extract_wwr_jobs
@@ -15,19 +14,16 @@ browser = webdriver.Chrome(options=options)
 base_url = "https://kr.indeed.com/jobs?q="
 search_term = "python"
 
-# indeed라는 웹사이트가 request를 막았기에 대신에 셀리니움을 쓴다.
 browser.get(f"{base_url}{search_term}")
 
-# .page_source로 페이지의 html을 가져올 수 있다.
-print(browser.page_source)
+soup = BeautifulSoup(browser.page_source, 'html.parser')
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+# recursive를 False로 하면 자식 내 요소는 찾지 않는다.
+jobs = job_list.find_all('li', recursive=False)
+for job in jobs:
+    # None도 변수에 넣을 수 있는 데이터 타입이다.
+    # None은 무언가가 없을 때 쓴다.
 
-# if response.status_code != 200:
-#     print("Can't request page :", response.status_code)
-# else:
-#     soup = BeautifulSoup(response.text, 'html.parser')
-#     job_list = soup.find("ul", class_="jobsearch-ResultsList")
-#     # recursive를 False로 하면 자식 내 요소는 찾지 않는다.
-#     jobs = job_list.find_all('li', recursive=False)
-#     for job in jobs:
-#         print(job)
-#         print("////////////////")
+    zone = job.find("div", class_="mosaic-zone")
+    if zone == None:
+        print("job li")
