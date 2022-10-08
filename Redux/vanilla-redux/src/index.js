20bd0d4b33a9ead9f8db8bc96007f46d88014044
arkk200 <arkk20000@gmail.com>
2022-10-09 00:48:55 +0900

    Store and Reducer

--- Redux/vanilla-redux/src/index.js
+++ Redux/vanilla-redux/src/index.js
@@ -1,22 +1,13 @@
+import { createStore } from "redux";
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
+const countModifier = (count = 0) => {
+  return count;
+};
 
-number.innerText = count;
+const countStore = createStore(countModifier);
 
-const updateText = () => {
-  number.innerText = count;
-}
-
-const handleAdd = () => {
-  count++;
-  updateText();
-}
-const handleMinus = () => {
-  count--;
-  updateText();
-}
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+console.log(countStore.getState());
