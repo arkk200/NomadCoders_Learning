feb3c16f9e1cf2261680863ccea9a3f714f97259
arkk200 <arkk20000@gmail.com>
2022-10-16 19:20:19 +0900

    Recap Refactor

--- Redux/vanilla-redux/src/index.js
+++ Redux/vanilla-redux/src/index.js
@@ -5,15 +5,18 @@ const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
 number.innerText = 0;
+// 변수로 action.type을 정의하면 에러를 볼 수 있다.
+const ADD = "ADD";
+const MINUS = "MINUS";
 
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if(action.type === "ADD") {
-    return count + 1;
-  } else if(action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch(action.type) { // if else 대신 switch문을 쓰는게 편하다.
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count - 1
+    default:
+      return count
   }
 };
 const countStore = createStore(countModifier);
@@ -27,5 +30,5 @@ countStore.subscribe(onChange)
 
 console.log(countStore.getState());
 
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }))
-minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }))
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }))
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }))
