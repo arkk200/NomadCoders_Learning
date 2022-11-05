f2897714875a79b865e918f74aa85f176b9f7a93
arkk200 <arkk20000@gmail.com>
2022-11-05 11:03:40 +0900

    Connecting the Store

--- Redux/vanilla-redux/src/index.js
+++ Redux/vanilla-redux/src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import App from './components/App';
 import { createRoot } from 'react-dom/client';
+import { Proveder } from 'react-redux';
+import store from './store';
 
-createRoot(document.getElementById('root')).render(<App></App>)
+createRoot(document.getElementById('root')).render(
+    <Proveder store={store}>
+        <App />
+    </Proveder>
+);
