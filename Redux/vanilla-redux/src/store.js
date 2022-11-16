70e5679b8de93d543a7c396762aabb4022335f20
arkk200 <arkk20000@gmail.com>
2022-11-16 11:22:46 +0900

    createSlice

--- Redux/vanilla-redux/src/store.js
+++ Redux/vanilla-redux/src/store.js
@@ -1,24 +1,16 @@
-// import { createStore } from "redux";
-import { configureStore, createAction, createReducer } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
+const toDos = createSlice({ // createSlice로 reducer, state, actions 다 만들 수 있다.
+    name: 'toDosReducer',
+    initialState: [],
+    reducers: {
+        add: (state, action) => {
+            state.push({ text: action.payload, id: Date.now() });
+        },
+        remove: (state, action) => state.filter(toDo => toDo.id !== action.payload)
+    }
+});
 
-const reducer = createReducer([], {
-    [addToDo]: (state, action) => {
-        // createReducer를 쓰면 state를 mutate할 수 있음
-        state.push({ text: action.payload, id: Date.now() });
-    },
-    [deleteToDo]: (state, action) => 
-        state.filter(toDo => toDo.id !== action.payload)
-})
+export const { add, remove } = toDos.actions;
 
-// configureStore는 chome Redux DevTools 확장 프로그램을 통해 브라우저에 개발 툴을 쓸 수 있게 해줌
-const store = configureStore({ reducer });
-
-export const actionCreators = {
-    addToDo,
-    deleteToDo
-}
-
-export default store;
+export default configureStore({ reducer: toDos.reducer });
