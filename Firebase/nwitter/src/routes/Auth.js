8778abc1cecd3f651e8df0726d165029ab170439
arkk200 <arkk20000@gmail.com>
2022-11-19 18:16:44 +0900

    Creating Account

--- Firebase/nwitter/src/routes/Auth.js
+++ Firebase/nwitter/src/routes/Auth.js
@@ -1,8 +1,12 @@
+import { async } from "@firebase/util";
 import { useState } from "react";
+import { authService } from "../fbase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 const Auth = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [newAccount, setNewAccount] = useState(true);
     const onChange = e => {
         const {target: {name, value}} = e;
         if (name === "email") {
@@ -11,8 +15,21 @@ const Auth = () => {
             setPassword(value);
         }
     }
-    const onSubmit = e => {
+    const onSubmit = async (e) => {
         e.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                // 계정 생성하기
+                data = await createUserWithEmailAndPassword(authService, email, password);
+            } else {
+                // 로그인하기
+                data = await signInWithEmailAndPassword(authService, email, password);
+            }
+            console.log(data);
+        } catch (err) {
+            console.log(err);
+        }
     }
     return (
         <div>
@@ -33,7 +50,7 @@ const Auth = () => {
                     value={password}
                     onChange={onChange}
                 />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
             </form>
             <div>
                 <button>Continue with Google</button>
