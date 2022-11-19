e2700fd4c9a47e253ab92791053c882eaffcefe3
arkk200 <arkk20000@gmail.com>
2022-11-19 15:05:51 +0900

    Securing the Keys

--- Firebase/nwitter/src/firebase.js
+++ Firebase/nwitter/src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBKPI4nSF0aqjNCISvOrwbr3qR_zZmiDZM",
-    authDomain: "nwitter-e9d44.firebaseapp.com",
-    projectId: "nwitter-e9d44",
-    storageBucket: "nwitter-e9d44.appspot.com",
-    messagingSenderId: "420567849050",
-    appId: "1:420567849050:web:d032b6d76709a653e4a063"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
