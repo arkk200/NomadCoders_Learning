f0ee5996a28911aeac0a957e48f1a1e1127d6e82
arkk200 <arkk20000@gmail.com>
2022-11-19 16:21:57 +0900

    Router Setup

--- Firebase/nwitter/src/firebase.js
+++ Firebase/nwitter/src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
+    // 이런식으로 Github에 내 firebase API key를 안보이게 할 수 있다.
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
