b795779c44565097478ff40211aabde53637d951
arkk200 <arkk20000@gmail.com>
2022-11-22 21:55:43 +0900

    Log Out

--- Firebase/nwitter/src/components/Router.js
+++ Firebase/nwitter/src/components/Router.js
@@ -1,14 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import Auth from 'routes/Auth';
+import Home from 'routes/Home';
+import Profile from 'routes/Profile';
+import Navigation from 'components/Navigation';
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {
-                    isLoggedIn ? 
-                    <Route path="/*" element={<Home />} />
+                    isLoggedIn ? <>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/profile" element={<Profile />} />
+                    </>
                     : <Route path="/*" element={<Auth />} />
                 }
             </Routes>
--- Firebase/nwitter/src/routes/Profile.js
+++ Firebase/nwitter/src/routes/Profile.js
@@ -1,1 +1,14 @@
-export default () => <span>Profile</span>;
+import { auth } from "fbase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+export default () => {
+    const navigate = useNavigate();
+    const onLogOutClick = () => {
+        signOut(auth); // signOut을 호출하면 sign out 된다.
+        navigate('/'); // 홈으로 이동한다.
+    };
+    return (
+        <button onClick={onLogOutClick}>Log Out</button>
+    )
+};
