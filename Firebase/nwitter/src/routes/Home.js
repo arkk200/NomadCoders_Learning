4fff145a57feea73db690c5084c46db7756e9a0b
arkk200 <arkk20000@gmail.com>
2022-11-25 07:56:25 +0900

    Getting the Nweets

--- Firebase/nwitter/src/routes/Home.js
+++ Firebase/nwitter/src/routes/Home.js
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "fbase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
 const Home = () => {
     const [nweet, setNweet] = useState('');
+    const [nweets, setNweets] = useState([]); // db에 저장된 nweets
+    const getNweets = async () => {
+        const dbNweets = await getDocs(collection(db, "nweets")); // getDocs로 docs를 가져올 수 있음
+        dbNweets.forEach(doc => {
+            const nweetObj = {
+                ...doc.data(),
+                id: doc.id // doc에 있는 고유 id를 각 nweet에게 줌
+            }
+            setNweets(prev => [nweetObj, ...prev]);
+        });
+    }
+    useEffect(() => {
+        getNweets();
+    }, []);
     const onSubmit = async (e) => {
         e.preventDefault();
         // getFirestore로 가져온 db를 
@@ -16,7 +30,7 @@ const Home = () => {
         setNweet("")
     }
     const onChange = e => {
-        const {target: {value}} = e;
+        const { target: { value } } = e;
         setNweet(value);
     }
     return (
@@ -31,6 +45,11 @@ const Home = () => {
                 />
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map(nweet => <div key={nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                </div>)}
+            </div>
         </div>
     )
 };
