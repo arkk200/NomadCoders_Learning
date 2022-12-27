80c32a65e90b2251fe65e41f46186714ab542e80
arkk200 <arkk20000@gmail.com>
2022-12-27 21:56:16 +0900

    Get My Own Nweets

--- Firebase/nwitter/src/routes/Profile.js
+++ Firebase/nwitter/src/routes/Profile.js
@@ -1,13 +1,27 @@
-import { auth } from "fbase";
+import { auth, db } from "fbase";
 import { signOut } from "firebase/auth";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default () => {
+export default ({ userObj }) => {
     const navigate = useNavigate();
     const onLogOutClick = () => {
         signOut(auth); // signOut을 호출하면 sign out 된다.
         navigate('/'); // 홈으로 이동한다.
     };
+    const getMyNweets = async() => {
+        const q = query( // 내가 단 nweets만 가져옴
+            collection(db, "nweets"),
+            where("creatorId", "==", userObj.uid),
+            orderBy("createdAt", "desc")
+        );
+        const querySnapshot = await getDocs(q);
+        console.log(querySnapshot.docs.map(doc => doc.data()));
+    }
+    useEffect(() => {
+        getMyNweets();
+    }, []);
     return (
         <button onClick={onLogOutClick}>Log Out</button>
     )
