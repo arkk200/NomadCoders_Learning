87c16d08a1318325e87d73a26d7a15f39454052d
arkk200 <arkk20000@gmail.com>
2022-12-28 22:05:33 +0900

    Update Profile

--- Firebase/nwitter/src/routes/Profile.js
+++ Firebase/nwitter/src/routes/Profile.js
@@ -1,15 +1,22 @@
+import { async } from "@firebase/util";
 import { auth, db } from "fbase";
-import { signOut } from "firebase/auth";
+import { signOut, updateProfile } from "firebase/auth";
 import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default ({ userObj }) => {
     const navigate = useNavigate();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const onLogOutClick = () => {
         signOut(auth); // signOut을 호출하면 sign out 된다.
-        navigate('/'); // 홈으로 이동한다.
+        setTimeout(() => {
+            navigate('/', {replace: true}); // 홈으로 이동한다.
+        }, 100);
     };
+    const onChange = e => {
+        setNewDisplayName(e.target.value);
+    }
     const getMyNweets = async() => {
         const q = query( // 내가 단 nweets만 가져옴
             collection(db, "nweets"),
@@ -22,7 +29,26 @@ export default ({ userObj }) => {
     useEffect(() => {
         getMyNweets();
     }, []);
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        if(userObj.displayName !== newDisplayName) {
+            await updateProfile(userObj, { // updateProfile로 user의 정보를 업데이트 시킬 수 있다.
+                displayName: newDisplayName
+            })
+        }
+    }
     return (
-        <button onClick={onLogOutClick}>Log Out</button>
+        <>
+            <form onSubmit={onSubmit}>
+                <input 
+                    value={newDisplayName}
+                    onChange={onChange}
+                    type="text" 
+                    placeholder="Display name" 
+                />
+                <input type="submit" value="Update Profile" />
+            </form>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
     )
 };
