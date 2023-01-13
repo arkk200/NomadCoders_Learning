9cbb6ac92c33c60e7d8851b263f46a8c484572af
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-01-14 02:18:27 +0900

    Chain

--- Typescript/typechain/src/index.ts
+++ Typescript/typechain/src/index.ts
@@ -22,5 +22,44 @@ class Block implements BlockShape {
 
     static calculateHash(prevHash: string, height: number, data: string): string {
         const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain {
+    private blocks: Block[];
+    constructor() {
+        this.blocks = [];
+    }
+    private getPrevHash() {
+        if(this.blocks.length === 0) return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    public addBlock(data: string) {
+        const newBlock = new Block(
+            this.getPrevHash(),
+            this.blocks.length,
+            data
+        );
+        this.blocks.push(newBlock);
+    }
+    public getBlocks() {
+        // 스프레드 연산자를 이용하여
+        // blockchain.getBlocks().push() 핵을
+        // 막을 수 있다.
+        return [...this.blocks];
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+// getBlocks()가 새로운 배열을 리턴하기에
+// private인 blocks엔 영향을 끼치지 않음
+blockchain.getBlocks().push(new Block("xxxxxx", 11111, "HACKEDDDDDD"));
+
+console.log(blockchain.getBlocks());
